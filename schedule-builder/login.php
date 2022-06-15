f372c27dd4b55ea6688493684e0deff707141cd8
nanaabaackah <nanaabaackah@trentu.ca>
2022-06-14 23:49:18 -0400

    files up-to-date

---
+++ schedule-builder/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <script src="scripts/index.js"></script>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Login</title>
+        <link rel="stylesheet" href="styles/login.css" />
+        <script src="https://kit.fontawesome.com/cd912461da.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <main>
+        </main>
+    </body>
+</html>
