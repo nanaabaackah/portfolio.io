a171a872a4282f0d9f9e5141138cc35bccbf54f8
nanaabaackah <nanaabaackah@trentu.ca>
2022-06-14 23:56:31 -0400

    Update login.php

--- schedule-builder/login.php
+++ schedule-builder/login.php
@@ -10,52 +10,7 @@
     </head>
     <body>
         <main>
-        <div class="left-container">
-                <div class="menu">
-                    <div id="logo">
-                        <a href="index.html"><i class="fa-solid fa-calendar-days"></i></a>
-                    </div>
-                </div>
-               <div id="description">
-                    <h1>Welcome to scheduleD.</h1>
-                    <h2>The Smartest Scheduling Software of its kind.</h2>
-               </div>
-                <div>
-                    <nav>
-                        <ul>
-                            <li><a href="">Privacy Policy</a></li>
-                            <li><a href="">Terms of Use</a></li>
-                            <li><a href="">Cookie Policy</a></li>
-                            <li><a href="">Facebook</a></li>
-                            <li><a href="">Twitter</a></li>
-                            <li><a href="">Instagram</a></li>
-                            <li><a href="">YouTube</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-            <div class="right-container">
-                <div id="pin">
-                    <nav>
-                        <ul>
-                            <li><a href="">LOGIN</a></li>
-                            <li><a href="">REGISTER</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div>
-                    <form>
-                        <input name="email" type="email" placeholder="Email" />
-                        <input name="password" type="password" placeholder="Password" />
-                        <button><a href="" >Login</a></button>
-                        
-                    </form>
-                </div>
-                <div id="forgot">
-                    <a href="">Forgotten your password?</a>
-                </div>
-                
-            </div>
+            <h1><?php echo "This is a page" ?></h2>
         </main>
     </body>
 </html>
