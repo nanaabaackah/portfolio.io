49c9c22dc60c85f10a9ef182d33f884e52b55b8f
nanaabaackah <nanaabaackah@trentu.ca>
2022-06-29 12:14:11 -0400

    done

--- project/scripts/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-window.addEventListener("DOMContentLoaded", ()=>{
-   let button1 = document.querySelector(".sign ");
-   button1.addEventListener('click', ()=>{
-        window.location.href="signup.php";
-   });
-})
