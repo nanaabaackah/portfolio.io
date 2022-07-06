d0fee60b307e222628e58cfd34b520f374a2ce4e
nanaabaackah <nanaabaackah@trentu.ca>
2022-07-05 20:34:31 -0400

    files updated

--- schedule-builder/register.php
+++ schedule-builder/register.php
@@ -5,7 +5,28 @@ $password = $_POST['pword'] ?? "";
 
 session_start();
 
-require "includes/library.php";
+$host = 'localhost';
+$username= 'root';
+$password='';
+$dbName = 'scheduling_website';
+$charset = 'utf8mb4';
+
+$con = mysqli_connect($host, $user, $password, $database);
+//If the connection fails, print out the error.
+if(mysqli_connect_errno($con)){
+
+	echo "Failed to connect to MySQL:".mysqli_connect_errno($con);
+	}
+	else{
+/*Otherwise, print out that it was a successful connection.	
+<h1> and <center> are HTML elements, however, this is not an HTML course we are just learning how to manipulate data.
+HTML is the Hypertext Markup Language	
+*/	
+	echo "<center><h1>Successful Connection</h1></center>";
+	}
+
+mysqli_close($con);
+/*require "includes/library.php";
 
 $pdo = connectDB();
 
@@ -14,5 +35,5 @@ $stmt = $pdo->prepare($query);
 $stmt->execute([$fullname, $email, $password]);
 
 header("Location:index.html");
-exit();
+exit();*/
 ?>
